5f4859cd937730637fdc204468fbb3156cf579c0
Felix <felix@lixdev.de>
2018-03-22 14:22:43 +0100

    performance issue removed

--- dist/greatstars.js
+++ dist/greatstars.js
@@ -5,7 +5,6 @@
  * Copyright by Lixdev (Felix Gutzmann)
  * Released under the MIT license
  */
-var stars;
 (function($) {
 
     $.fn.greatStars = function(obj) {
@@ -16,10 +15,10 @@ var stars;
             number: 100,
             speed: 50,
             starClass: "star",
-            direction: "up"
+            direction: "up",
+            levelClass: "level",
+            steps: 2
         };
-
-        var interval;
 
         this.updateSettings = function(obj) {
 
@@ -44,81 +43,133 @@ var stars;
             
             return res;
         }
-        
-        this.createStars = function() {
 
-            for(var i=0; i<this.settings.number; i++) {
+        this.createLevels = function() {
 
-                var iClass = this.settings.starClass+" "+this.settings.starClass+"-"+(i+1);
-                var PosLeft = this.rndIntRange(0, $(this).width());
-                var PosTop = this.rndIntRange(0, $(this).height());
-            
-                var hwrange = this.rndIntRange(1, 5);
-                var width = hwrange + "px";
-                var height = hwrange + "px";
-                var element = "."+iClass.replace(" ", ".");
+            for(var i=0; i < 2; i++) {
                 
-                $(this).append('<i class="'+iClass+'"></i>');
-                
-                this.stars.push({
-                    element: iClass,
-                    top: PosTop, 
-                    left: PosLeft,
-                    height: height,
-                    width: width
-                });
-                
-                $(element).css({
-                    "position": "absolute",
-                    "left":PosLeft + "px", 
-                    "top": PosTop + "px", 
-                    "height": height, 
-                    "width": width
-                });
-                
-            }
-        }, this.createStars();
-        
+                var lClass = this.settings.levelClass + " " + this.settings.levelClass + "-" + (i+1);
 
-        this.moveStars = function() {
-                
-            for(var i=0; i < this.stars.length; i++) {
-   
-                var element = "."+this.stars[i].element.replace(" ", ".");
+                $(this).append('<div class="'+lClass+'"></div>');
 
-                if(this.settings.direction == "up") {
+                var elem = $("."+lClass.replace(" ", "."));
 
-                    if(this.stars[i].top < 0) {
-                        this.stars[i].top = $(this).height() + 20;
-                        this.stars[i].left = this.rndIntRange(0, this.width()); 
-                    } else {
-                        this.stars[i].top = this.stars[i].top - 1;
+                if(i<1) {
+
+                    
+                    var level = {
+                        "element": elem,
+                        "stars": [],
+                        "yPos": 0
                     }
                     
-                } else if (this.settings.direction == "down") {
+                    this.stars.push(level);
                     
-                    if(this.stars[i].top > this.height()) {
-                        this.stars[i].top = $(this).offset().top - 20;
-                        this.stars[i].left = this.rndIntRange(0, this.width()); 
-                    } else {
-                        this.stars[i].top = this.stars[i].top + 1;
+                    elem.css({"position":"absolute", "top":level.yPos+"px"});
+
+                } else {   
+
+                    var level = {
+                        "element": elem,
+                        "stars": [],
+                        "yPos": this.height()
                     }
+    
+                    this.stars.push(level);
+
+                    elem.css({"position":"absolute", "top":level.yPos+"px"});
 
                 }
-
-                stars = this.stars;
                 
-                $(element).css({ "top": this.stars[i].top, "left": this.stars[i].left});
-
             }
+
+        }, this.createLevels();
+
+        this.moveLevels = function() {
+
+            for(var i=0; i < this.stars.length; i++) {
+
+                var elem = this.stars[i].element;
+
+                if (this.settings.direction == "up") {
+
+                    if(this.stars[i].yPos< (0 - this.height())) {
+
+                        this.stars[i].yPos = (this.height() - 1); 
+
+                    }
+                    
+                    var newPos = this.stars[i].yPos - this.settings.steps;
+                    
+                    elem.css({"top": newPos+"px"});
+                    
+                    this.stars[i].yPos = newPos;
+                    
+                } else if(this.settings.direction == "down") {
+
+                    
+                    if(this.stars[i].yPos> this.height()) {
+
+                        this.stars[i].yPos = ((this.scrollTop() - this.height()) + 1);    
+                                            
+                    }
+                    
+                    var newPos = this.stars[i].yPos + this.settings.steps;
+                    
+                    elem.css({"top": newPos+"px"});
+                    
+                    this.stars[i].yPos = newPos;
+                }
+    
+            }
+
 
             var self = this;
             interval = setTimeout(function() {
-                self.moveStars();
+                self.moveLevels();
             },this.settings.speed);
+
+        }, this.moveLevels();
+
+        this.createStars = function() {
+            
+            for(var i=0; i<this.settings.number; i++) {
                 
-        }, this.moveStars();
-        
+                var iClass = this.settings.starClass+" "+this.settings.starClass+"-"+(i+1);
+
+                for(var z=0; z < 2; z++) {
+
+                    var PosLeft = this.rndIntRange(0, $(this).width());
+                    var PosTop = this.rndIntRange(0, $(this).height());
+                    
+                    var hwrange = this.rndIntRange(1, 5);
+                    var width = hwrange + "px";
+                    var height = hwrange + "px";
+                    var element = "."+iClass.replace(" ", ".");
+
+                    $(this.stars[z].element).append('<i class="'+iClass+'"></i>');
+                    
+                    this.stars[z].stars.push({
+                        element: iClass,
+                        top: PosTop, 
+                        left: PosLeft,
+                        height: height,
+                        width: width
+                    });
+                    
+                    $(element).css({
+                        "position": "absolute",
+                        "left":PosLeft + "px", 
+                        "top": PosTop + "px", 
+                        "height": height, 
+                        "width": width
+                    });
+         
+                }
+                
+            }
+        }, this.createStars();
+
         this.stop = function() {
             clearInterval(interval);
             return "Stopped moving stars";
